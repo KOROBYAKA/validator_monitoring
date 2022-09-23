5e130b6fa7e4348e3fc757540ed4a7c6af741561
KOROBYAKA <wads31566@gmail.com>
2022-09-23 14:30:05 +0300

    3'rd commit

--- main.py
+++ main.py
@@ -12,44 +12,19 @@ from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.topo import SingleSwitchTopo
 from mininet.log import setLogLevel
-class VLANHost( Host ):
-    "Host connected to VLAN interface"
-
-    # pylint: disable=arguments-differ
-    def config( self, vlan=100, **params ):
-        """Configure VLANHost according to (optional) parameters:
-           vlan: VLAN ID for default interface"""
-
-        r = super( VLANHost, self ).config( **params )
-
-        intf = self.defaultIntf()
-        self.cmd( 'ifconfig %s inet 0' % intf )
-        self.cmd( 'vconfig add %s %d' % ( intf, vlan ) )
-        self.cmd( 'ifconfig %s.%d inet %s' % ( intf, vlan, params['ip'] ) )
-        newName = '%s.%d' % ( intf, vlan )
-        intf.name = newName
-        self.nameToIntf[ newName ] = intf
-
-
-        return r
-
-
-hosts = { 'vlan': VLANHost }
-
-
 
 
 class MyTopo( Topo ):
 
-    def build( self, vlanBase=100 ):
+    def build( self):
         s1 = self.addSwitch('s1')
         hB = self.addHost('hB')
 
-        vlan = vlanBase
-        hA = self.addHost('hA',cls=VLANHost,vlan=vlan)
 
-        x = 1
-        for x in range (4):
+        hA = self.addHost('hA')
+
+
+        for x in range (0,4):
             self.addLink('hB', 's1')
         self.addLink('hA', 's1')
 
@@ -63,14 +38,18 @@ def test():
 
 
     hA = net['hA']
-
+    hA.cmd(f'ip link add name br0 type bridge')
     for x in range (0,4):
         hA.cmd(f'sudo ip link add link hA-eth0 name hA-eth0.10{x} type vlan id 10{x}')
-        hA.cmd(f'ip addr add 10.0.0.{x}/24 brd 10.0.0.255 dev hA-eth0.10{x}')
         hA.cmd(f'ip link set dev hA-eth0.10{x} up')
-     #   s1.cmd('')
-      #  s1.cmd('')
-       # s1.cmd('')
+        hA.cmd(f'ip link set hA-eth0.10{x} master br0')
+        ''' ping po IP rabotaet hA.cmd(f'ip address add dev hA-eth0.10{x} 10.0.0.2{x}')'''
+    hA.cmd('ip address add dev br0 10.0.0.111/24')
+    hA.cmd('ip address add dev hA-eth0 10.0.0.11/24')
+    hA.cmd('ip link set br0 up')
+    hA.cmd('ip link set hA-eth0 up')
+
+
     CLI(net)
     net.stop()
 def main():
