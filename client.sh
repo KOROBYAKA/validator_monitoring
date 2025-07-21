fc4a2359d1e4d4d3ca9a2ae93db640cb4f4eab16
Aleksei Zuev <wads31566@gmail.com>
2025-07-21 13:58:34 -0400

    Added:mock server & integrated

--- client.sh
+++ client.sh
@@ -6,18 +6,18 @@ CLI="ip netns exec client${TARGET}"
 LOSS_PERCENT=$5 #namespace link loss percentage to server connection
 SRV="ip netns exec server"
 
-echo "Creating a namespace client${TARGET}"
-ip netns del client$TARGET
-ip netns add client$TARGET
+echo "Creating a namespace client ${TARGET}"
+ip netns del client${TARGET}
+ip netns add client${TARGET}
 
 echo "Set up link between client${TARGET} and server namespaces"
-ip link add veth_srv-$TARGET type veth peer name veth-$TARGET
-ip link set dev veth-${TARGET} netns client$TARGET
-ip link set dev veth_srv-$TARGET netns server
-$SRV ip link set veth_srv-$TARGET master srv-br
-$SRV ip link set veth_srv-$TARGET up
-$CLI ip a a 10.0.1.$TARGET_IP/24 dev veth-$TARGET
-$CLI ip link set veth-$TARGET up
+ip link add veth_srv-${TARGET} type veth peer name veth-${TARGET}
+ip link set dev veth-${TARGET} netns client${TARGET}
+ip link set dev veth_srv-${TARGET} netns server
+$SRV ip link set veth_srv-${TARGET} master srv-br
+$SRV ip link set veth_srv-${TARGET} up
+$CLI ip a a 10.0.1.${TARGET_IP}/24 dev veth-${TARGET}
+$CLI ip link set veth-${TARGET} up
 echo "Connectivity check"
 $CLI ping 10.0.1.1 -c 1 -W 0.00001
 
@@ -28,5 +28,5 @@ $SRV tc qdisc add dev veth_srv-${TARGET} parent 1: handle 2: netem loss ${LOSS_P
 
 $CLI tc qdisc add dev veth-${TARGET} root handle 1: netem delay ${DELAY_MS}ms ${DELAY_DISTRIBUTION_MS}ms distribution normal
 $CLI tc qdisc add dev veth-${TARGET} parent 1: handle 2: netem loss ${LOSS_PERCENT} rate 100Mbit corrupt 1% duplicate 1%
-echo 'Run "ip netns exec client${TARGET} bash" to start a shell in namespace for accessing client-${TARGET}'
+echo 'Run "ip netns exec client ${TARGET} bash" to start a shell in namespace for accessing client-${TARGET}'
 echo 'Internet access is not configured inside the namespaces.'
--- sosim.py
+++ sosim.py
@@ -3,13 +3,22 @@ import subprocess
 import argparse
 
 
-def get_label(index):
-    """
-    Converts a 0-based index to a letter ID like A, B, ..., Z, AA, AB, ..., etc.
-    """
-    result = ""
-    index += 1  # To make it 1-based like Excel
+class Node():
+    def __init__(self, ip:str, label:str, target_ip:str):
+        self.ip = ip
+        self.label = label
+        self.target_ip = target_ip
 
+    def run_agave_client(self):
+        cli = f"ip netns exec client{self.label}"
+        args = f"--target {self.target_ip}:8009 --duration 600 --num-connections 4"
+        proc = subprocess.Popen(f"{cli} ./mock_server/target/debug/client {args}",
+                                shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+                                )
+
+def get_label(index):
+    result = ""
+    index += 1
     while index > 0:
         index, remainder = divmod(index - 1, 26)
         result = chr(65 + remainder) + result
@@ -24,13 +33,12 @@ def main():
     #Link qualities
     link_delays = [35,100,200]
     delay_distributions = [15,45,50]
-    shift = 0
     parser = argparse.ArgumentParser(prog='sosim',description="Solana validator Simulation",
                                      epilog="If you encounter some bug, I wish you a luck ©No-Manuel Macros")
     parser.add_argument('hosts_amount')
     parser.add_argument('loss_percentage')
     args = parser.parse_args()
-
+    nodes = [Node("10.0.1.2","A","10.0.1.1")]
     print("Creating server namespace with a single host")
     subprocess.run("./server.sh", shell=True)
     for x in range(1,int(args.hosts_amount)):
@@ -38,9 +46,21 @@ def main():
         link_delay = link_delays[(x//len(link_delays)-1)]
         delay_distribution = delay_distributions[(x//len(delay_distributions)-1)]
         print(f"./client.sh {name} {x} {link_delay} {delay_distribution} {args.loss_percentage}")
+        nodes.append(Node(f"10.0.1{x+2}",name,"10.0.1.1"))
         subprocess.run(f"./client.sh {name} {x+2} {link_delay} {delay_distribution} {args.loss_percentage}",shell=True)
 
-    print("success")
+
+    print("Environment is up.\nRunning a server")
+    #run mock-server
+    cli = f"ip netns exec server"
+    args = f"--listen 10.0.1.1:8009 --receive-window-size 630784  --max-concurrent-streams 512 --stream-receive-window-size 1232"
+    proc = subprocess.Popen(f"{cli} ./mock_server/target/debug/server {args}",
+        shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+
+    for node in nodes:
+        node.run_agave_client()
+
 
 
 
